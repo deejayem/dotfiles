22112732f12414b8c193a30eed7987fff0869414
David Morgan <djm_uk@protonmail.com>
2022-09-16 06:50:50 +0100

    Only use keychain on linux

--- nix-conf/home/zsh.nix
+++ nix-conf/home/zsh.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.packages = with pkgs; [
@@ -18,7 +18,7 @@
     nix-direnv.enable = true;
   };
   programs.keychain = {
-    enable = true;
+    enable = lib.mkIf pkgs.stdenv.isLinux true;
     agents = [ "ssh" "gpg"];
     keys = [ "id_rsa" "id_ed25519" "C171251002C200F2" ];
   #  extraFlags = [ "--quiet" "--ignore-missing" ];
