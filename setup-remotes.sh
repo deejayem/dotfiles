3e0e6d5518af1bb1894625427541d6b040e4d4d5
David Morgan <djm_uk@protonmail.com>
2022-10-22 14:49:51 +0100

    Add setup-remotes script

---
+++ setup-remotes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git remote add sourcehut git@git.sr.ht:~djm/dotfiles
+git remote add codeberg git@codeberg.org:djm/dotfiles.git
+git remote add tildegit git@tildegit.org:djm/dotfiles.git
+git remote add tilde.institute djm@tilde.institute:public_repos/dotfiles
+git remote add github git@github.com:deejayem/dotfiles.git
+
+for i in `git remote -v | grep -v origin|awk '{print $2}' | uniq`; do git remote set-url --add --push origin $i ; done
+
