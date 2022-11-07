fbc3a09b2beda6c8af794a879bf2e7fe58563546
David Morgan <djm_uk@protonmail.com>
2022-11-07 21:20:49 +0000

    Play with tmux conf some more

--- nix-conf/home/includes/linux-server.nix
+++ nix-conf/home/includes/linux-server.nix
@@ -24,15 +24,21 @@
     prefix = "C-a";
     #tmuxp.enable = true;
     extraConfig = ''
-        setw -g window-status-current-format "#[fg=red,bold][#[fg=default]#F#I:#W#F#[fg=red,bold]]#[default]"
-        setw -g window-status-format "#[fg=green]{#[default]#F#I:#W#F#[fg=green]}#[default]"
+      bind-key C-a last-window
+      bind-key a send-prefix
 
-        set -g status-left-length 17
+      set -g status-bg black
+      set -g status-fg white
 
-        set -g status-interval 1
+      setw -g window-status-current-format "#[fg=red,bold][#[fg=default]#F#I:#W#F#[fg=red,bold]]#[default]"
+      setw -g window-status-format "#[fg=green]{#[default]#F#I:#W#F#[fg=green]}#[default]"
 
-        set -g status-left "#[fg=yellow]#h#[default]"
-        set -g status-right "#[fg=magenta,bold]#(/home/djm/bin/showmail.sh)#[fg=blue]%a%d/%m#[fg=yellow]%H:%M:%S"
+      set -g status-left-length 17
+
+      set -g status-interval 1
+
+      set -g status-left "#[fg=yellow]#h#[default]"
+      set -g status-right "#[fg=magenta,bold]#(/home/djm/bin/showmail.sh)#[fg=blue]%a%d/%m#[fg=yellow]%H:%M:%S"
     '';
   };
 
