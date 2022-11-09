f208e67d997b848036a51a3ac45166a5576a83c4
David Morgan <djm_uk@protonmail.com>
2022-11-09 16:39:36 +0000

    Add home-setup.sh

---
+++ setup-home.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+git clone git@codeberg.org:djm/dotfiles.git
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-$(nixos-version | cut -d. -f1-2).tar.gz home-manager
+nix-channel --update
+
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+nix-shell '<home-manager>' -A install
+
+[ -f ~/dotfiles/nix-conf/home/$HOST.nix ] && ln -sf ~/dotfiles/nix-conf/home/$HOST.nix ~/.config/nixpkgs/home.nix
+ln -sf ~/dotfiles/.p10k.zsh ~/
+ln -sf ~/dotfiles/.emacs.d ~/
+
+home-manager switch
+
