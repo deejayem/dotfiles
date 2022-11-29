db5ca9400c1973f9cba23503ed1824770c40e5f9
David Morgan <djm_uk@protonmail.com>
2022-11-29 16:15:11 +0000

    Update tramp config

--- .emacs.d/lisp/init-tramp.el
+++ .emacs.d/lisp/init-tramp.el
@@ -4,13 +4,14 @@
 
 (use-feature tramp
   :defer 5
+  :config
+  (add-to-list 'tramp-remote-path "/run/current-system/sw/bin")
   :custom
   (tramp-default-method "ssh")
   (vc-ignore-dir-regexp
    (format "\\(%s\\)\\|\\(%s\\)"
            vc-ignore-dir-regexp
            tramp-file-name-regexp))
-  (tramp-default-method "ssh")
   (tramp-auto-save-directory (expand-file-name "tramp-auto-save" user-emacs-directory))
   (tramp-persistency-file-name (expand-file-name "tramp-connection-history" user-emacs-directory))
   (password-cache-expiry nil))
