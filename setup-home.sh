259a6ed52e62ae0da44295ecf6b0840c1172c4d3
David Morgan <djm_uk@protonmail.com>
2022-11-30 16:17:07 +0000

    Add correct channels

--- setup-home.sh
+++ setup-home.sh
@@ -2,7 +2,16 @@
 
 [ -e ~/dotfiles ] || git clone git@codeberg.org:djm/dotfiles.git
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-$(nixos-version | cut -d. -f1-2).tar.gz home-manager
+if [ -x "$(command -v nixos-version)" ]; then
+  # On NixOS use the hm version corresponding to the NixOS version, and add nixos-unstable as unstable
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-$(nixos-version | cut -d. -f1-2).tar.gz home-manager
+  nix-channel --add https://nixos.org/channels/nixos-unstable unstable
+else
+  # On other systems use master, and add nixpkgs-unstable as unstable for compatibility between NixOS and non-NixOS installations
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
+fi
+
 nix-channel --update
 
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
