c4a71cbc90b8ce101fbdca64231fb6536d8e9c0d
David Morgan <djm_uk@protonmail.com>
2022-12-02 12:23:06 +0000

    Improve fzf options

--- nix-conf/home/includes/zsh.nix
+++ nix-conf/home/includes/zsh.nix
@@ -104,7 +104,9 @@
 
       export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
       export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-      export FZF_ALT_C_COMMAND="rg --hidden --files --sort-files --null | xargs -0 dirname | sort -u"
+      export FZF_ALT_C_COMMAND='rg --hidden --files --sort-files --null -g ""!{.git,node_modules}/*" | xargs -0 dirname | sort -u'
+      export FZF_ALT_C_OPTS="--preview 'exa --tree {} | head -200'"
+      export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind 'ctrl-t:toggle-preview'"
 
       autopair-init
       enable-fzf-tab
