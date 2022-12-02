c1939b83ac661e776bfe4c0cf74731d3635784bf
David Morgan <djm_uk@protonmail.com>
2022-12-02 20:50:57 +0000

    Tidy up

--- nix-conf/home/includes/linux-dev.nix
+++ nix-conf/home/includes/linux-dev.nix
@@ -3,31 +3,17 @@
   imports = [
     ./dev-common.nix
   ];
-  #services.emacs.package = pkgs.emacsUnstable;
 
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-    }))
-  ];
   programs.emacs = {
     enable = true;
-    #package = pkgs.emacsGcc;
     package = pkgs.emacs28NativeComp;
     extraPackages = (epkgs: [ epkgs.vterm ] );
   };
 
   home.packages = with pkgs; [
-    docker
-    docker-compose
-    gcc
-    gnumake
     mpv
     mu
-    notmuch
-    nix
     protonvpn-cli
-    xsel
     youtube-dl
   ];
 
