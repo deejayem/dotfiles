2376adf93aab6e3cfe25ac487653c9ab78b41e2e
David Morgan <djm_uk@protonmail.com>
2023-01-17 13:21:19 +0000

    Fix indentation

--- .emacs.d/lisp/init-completion.el
+++ .emacs.d/lisp/init-completion.el
@@ -14,17 +14,17 @@
 (use-feature hippie-expand
   :config
   (setq hippie-expand-try-functions-list
-      '(;yas-hippie-try-expand
-        try-expand-dabbrev
-        try-expand-all-abbrevs
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-list
-        try-expand-line
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
+        '(;yas-hippie-try-expand
+          try-expand-dabbrev
+          try-expand-all-abbrevs
+          try-expand-dabbrev-all-buffers
+          try-expand-dabbrev-from-kill
+          try-complete-file-name-partially
+          try-complete-file-name
+          try-expand-list
+          try-expand-line
+          try-complete-lisp-symbol-partially
+          try-complete-lisp-symbol))
   ;; https://www.emacswiki.org/emacs/HippieExpand#h5o-9
   (defadvice he-substitute-string (after he-paredit-fix)
     "Remove extra paren when expanding line in paredit."
