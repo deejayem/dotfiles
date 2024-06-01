78a00c3a0b3f4e0ff372647564c30d78ce87d3a6
David Morgan <djm_uk@protonmail.com>
2024-06-01 15:46:05 +0100

    Fix indentation

--- .emacs.d/lisp/init-clojure.el
+++ .emacs.d/lisp/init-clojure.el
@@ -32,18 +32,18 @@
 
   ;; https://github.com/weavejester/compojure/wiki/Emacs-indentation
   (define-clojure-indent
-    (defroutes 'defun)
-    (GET 2)
-    (POST 2)
-    (PUT 2)
-    (DELETE 2)
-    (HEAD 2)
-    (ANY 2)
-    (OPTIONS 2)
-    (PATCH 2)
-    (rfn 2)
-    (let-routes 1)
-    (context 2))
+   (defroutes 'defun)
+   (GET 2)
+   (POST 2)
+   (PUT 2)
+   (DELETE 2)
+   (HEAD 2)
+   (ANY 2)
+   (OPTIONS 2)
+   (PATCH 2)
+   (rfn 2)
+   (let-routes 1)
+   (context 2))
 
   :init
   ;; Always show more of the path in clj buffer names.
