8e8f1fffa42e10c2d0af8fcc570c006227310dbe
David Morgan <djm_uk@protonmail.com>
2024-06-05 16:28:11 +0100

    Add dynamicForwards

--- nix-conf/home/includes/common.nix
+++ nix-conf/home/includes/common.nix
@@ -133,6 +133,7 @@ in
       "djm.ovh" = {
         hostname = "v.djm.ovh";
         port = 2222;
+        dynamicForwards = [ { port = 8889; } ];
       };
       "devio" = {
          hostname = "devio.us";
