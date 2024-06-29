8868d632656661a12a3ff7cdc4f955544ade0da1
David Morgan <djm_uk@protonmail.com>
2024-06-29 14:23:03 +0100

    Add user to disk group

--- nix-conf/machines/egalmoth/configuration.nix
+++ nix-conf/machines/egalmoth/configuration.nix
@@ -121,7 +121,7 @@
   users.users.djm =
    { isNormalUser = true;
      description = "David Morgan";
-     extraGroups = [ "wheel" "networkmanager" "scanner" "lp" "plocate" "cdrom" ];
+     extraGroups = [ "wheel" "networkmanager" "scanner" "lp" "plocate" "cdrom" "disk" ];
      shell = pkgs.zsh;
      openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCurCpxZCHtByB5wXzsjTXwMyDSB4+B8rq5XY6EGss58NwD8jc5cII4i+QUbCOGTiAggSZUSC9YIP24hjpOeNT/IYs5m7Qn1B9MtBAiUSrIYew8eDwnMLlPzN+k2x9zCrJeCHIvGJaFHPXTh1Lf5Jt2fPVGW9lksE/XUVOe6ht4N/b+nqqszXFhc8Ug6le2bC1YeTCVEf8pjlh/I7DkDBl6IB8uEXc3X2vxxbV0Z4vlBrFkkAywcD3j5VlS/QYfBr4BICNmq/sO3fMkbMbtAPwuFxeL4+h6426AARQZiSS0qVEc8OoFRBVx3GEH5fqVAWfB1geyLzei22HbjUcT9+xN davidmo@gendros"
