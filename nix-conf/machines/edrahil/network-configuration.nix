f5d98f9ea7acb863f527526cb837b5c66a345b63
David Morgan <djm_uk@protonmail.com>
2024-08-30 12:19:22 +0100

    Use tee for redirection instead of su -c

--- nix-conf/machines/edrahil/network-configuration.nix
+++ nix-conf/machines/edrahil/network-configuration.nix
@@ -5,7 +5,7 @@
       # Emulate nix-sops. Technically an anti-pattern, but IP addresses aren't real secrets, and this has to be embedded here,
       # as we cannot set a file path to read it from.
       # Populate/update with:
-      # doas su -c "SOPS_AGE_KEY=$(doas ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key) `which sops` -d --extract '[\"ipv6_address\"]' secrets/edrahil.yaml > /root/.config/secrets/ipv6_address"
+      # SOPS_AGE_KEY=$(doas ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key) sops -d --extract '["ipv6_address"]' secrets/edrahil.yaml | doas tee /root/.config/secrets/ipv6_address
       address = builtins.readFile "/root/.config/secrets/ipv6_address";
       prefixLength = 64;
     }];
