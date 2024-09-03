31e462e924c50523e0a37069eb7c043c48b0f390
David Morgan <djm_uk@protonmail.com>
2024-09-03 15:50:37 +0100

    Format scripts with nixfmt

--- nix-conf/home/includes/scripts/hm-changes-report.nix
+++ nix-conf/home/includes/scripts/hm-changes-report.nix
@@ -2,9 +2,9 @@
 
 # https://github.com/gvolpe/nix-config/blob/e28a220d0087064e6bad6b992b4914a65eb545e5/home/scripts/changes-report.nix
 let
-  hm-profiles = "${config.home.homeDirectory}/.local/state/nix/profiles/home-manager-*-link";
-in
-pkgs.writeShellScriptBin "hm-changes-report" ''
+  hm-profiles =
+    "${config.home.homeDirectory}/.local/state/nix/profiles/home-manager-*-link";
+in pkgs.writeShellScriptBin "hm-changes-report" ''
   # Disable nvd if there are less than 2 hm profiles.
   if [ $(ls -d1v ${hm-profiles} 2>/dev/null | wc -l) -lt 2 ]; then
     echo "Skipping changes report..."
