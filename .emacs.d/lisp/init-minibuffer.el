5551196d9c2a55a9478f17d83a316439f07ef767
David Morgan <djm_uk@protonmail.com>
2024-09-26 16:02:16 +0100

    Add comment and remove blank lines

--- .emacs.d/lisp/init-minibuffer.el
+++ .emacs.d/lisp/init-minibuffer.el
@@ -51,24 +51,22 @@
                     (if (not (eq 1 (abs (- beg-index vertico--index))))
                         (ding)))))
 
+  ;; https://github.com/minad/vertico/wiki#useful-commands-from-outside-minibuffer
   (defun down-from-outside ()
     "Move to next candidate in minibuffer, even when minibuffer isn't selected."
     (interactive)
     (with-selected-window (active-minibuffer-window)
       (execute-kbd-macro [down])))
-
   (defun up-from-outside ()
     "Move to previous candidate in minibuffer, even when minibuffer isn't selected."
     (interactive)
     (with-selected-window (active-minibuffer-window)
       (execute-kbd-macro [up])))
-
   (defun preview-from-outside ()
     "Preview the selected candidate, even when minibuffer isn't selected."
     (interactive)
     (with-selected-window (active-minibuffer-window)
       (execute-kbd-macro (kbd "M-."))))
-
   (defun to-and-fro-minibuffer ()
     "Go back and forth between minibuffer and other window."
     (interactive)
