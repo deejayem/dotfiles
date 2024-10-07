9064e27e28aacf4235a226987818a107dfa30ad3
David Morgan <djm_uk@protonmail.com>
2024-10-07 14:02:09 +0100

    Tidy hippie-expand-try-functions-list

--- .emacs.d/lisp/init-completion.el
+++ .emacs.d/lisp/init-completion.el
@@ -24,19 +24,18 @@
   (dabbrev-case-replace nil))
 
 (use-feature hippie-expand
+  :custom
+  (hippie-expand-try-functions-list '(try-expand-dabbrev
+                                      try-expand-all-abbrevs
+                                      try-expand-dabbrev-all-buffers
+                                      try-expand-dabbrev-from-kill
+                                      try-complete-file-name-partially
+                                      try-complete-file-name
+                                      try-expand-list
+                                      try-expand-line
+                                      try-complete-lisp-symbol-partially
+                                      try-complete-lisp-symbol))
   :config
-  (setq hippie-expand-try-functions-list
-        '(;yas-hippie-try-expand
-          try-expand-dabbrev
-          try-expand-all-abbrevs
-          try-expand-dabbrev-all-buffers
-          try-expand-dabbrev-from-kill
-          try-complete-file-name-partially
-          try-complete-file-name
-          try-expand-list
-          try-expand-line
-          try-complete-lisp-symbol-partially
-          try-complete-lisp-symbol))
   ;; https://www.emacswiki.org/emacs/HippieExpand#h5o-9
   (defadvice he-substitute-string (after he-paredit-fix)
     "Remove extra paren when expanding line in paredit."
