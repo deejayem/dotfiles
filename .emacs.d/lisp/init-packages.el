7e910aab85b1d9fc1e0cceb54238a01a9bf16e76
David Morgan <djm_uk@protonmail.com>
2024-11-22 12:58:54 +0000

    Prevent elpaca from being reloaded on update

--- .emacs.d/lisp/init-packages.el
+++ .emacs.d/lisp/init-packages.el
@@ -169,7 +169,7 @@ using this command."
   (cl-letf (((symbol-function 'yes-or-no-p) (cl-constantly t)))
     (when (not (or (memq id elpaca-ignored-dependencies)
                    ;; TODO why aren't xref and project already in the list?
-                   (memq id '(xref project perspective))))
+                   (memq id '(xref project perspective elpaca))))
       (+elpaca-reload-package id))))
 
 ;; https://github.com/radian-software/radian/blob/e3aad124c8e0cc870ed09da8b3a4905d01e49769/emacs/radian.el#L352
