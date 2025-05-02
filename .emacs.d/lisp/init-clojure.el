b29444ed523d73c0a3b68bd07cbeac5d682f6eb0
David Morgan <djm_uk@protonmail.com>
2025-05-02 20:27:29 +0100

    Bind cider-pprint-eval-last-sexp-to-comment

--- .emacs.d/lisp/init-clojure.el
+++ .emacs.d/lisp/init-clojure.el
@@ -214,6 +214,7 @@
   (:map cider-mode-map
         ("C-c M-l" . cider-load-file)
         ("C-c M-b" . cider-interrupt)
+        ("C-c C-j C-;" . cider-pprint-eval-last-sexp-to-comment)
         ("C-x M-i e" . cider-inspect-last-sexp)
         ("C-x M-i f" . cider-inspect-defun-at-point)
         ("C-x M-i l" . cider-inspect-last-result)
