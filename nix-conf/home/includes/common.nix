705d87abd1161f0174b5e98aeec06d9f6e858b3c
David Morgan <djm_uk@protonmail.com>
2025-07-29 12:18:58 +0100

    Fix aspell setup

--- nix-conf/home/includes/common.nix
+++ nix-conf/home/includes/common.nix
@@ -42,10 +42,7 @@ in
     hcr
     scr
 
-    aspell
-    aspellDicts.en
-    aspellDicts.en-computers
-    aspellDicts.en-science
+    (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
     bandwhich
     bottom
     broot
