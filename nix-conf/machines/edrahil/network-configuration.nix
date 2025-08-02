297ddae30ebe875f027f2e7b0ddfb56b858d876f
David Morgan <djm_uk@protonmail.com>
2025-08-02 21:04:43 +0100

    Move some network config to private.nix
    Move networking.interfaces.ens3.ipv6.addresses

--- nix-conf/machines/edrahil/network-configuration.nix
+++ nix-conf/machines/edrahil/network-configuration.nix
@@ -1,19 +1,12 @@
 { ... }:
 {
-  networking = {
-    interfaces.ens3.ipv6.addresses = [
-      {
-        # Emulate nix-sops. Technically an anti-pattern, but IP addresses aren't real secrets, and this has to be embedded here,
-        # as we cannot set a file path to read it from.
-        # Populate/update with:
-        # SOPS_AGE_KEY=$(doas ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key) sops -d --extract '["ipv6_address"]' machines/edrahil/secrets.yaml | doas tee /root/.config/secrets/ipv6_address
-        address = builtins.readFile "/root/.config/secrets/ipv6_address";
-        prefixLength = 64;
-      }
-    ];
-    defaultGateway6 = {
-      address = "fe80::1";
-      interface = "ens3";
-    };
+  imports = [
+    ./private.nix
+  ];
+
+  # networking.interfaces.ens3.ipv6.addresses configured in private.nix
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "ens3";
   };
 }
