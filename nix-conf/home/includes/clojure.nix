ab7defff6094af1150295f9974bcbda41555251b
David Morgan <djm_uk@protonmail.com>
2025-08-11 17:36:57 +0100

    Remove obsolete lib.optionals check

--- nix-conf/home/includes/clojure.nix
+++ nix-conf/home/includes/clojure.nix
@@ -1,25 +1,20 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }:
-let
-  inherit (lib) optionals;
-in
 {
   home.packages =
     with pkgs;
     [
       babashka
       clj-kondo
+      clojure
       clojure-lsp
       emacs-lsp-booster
       jet
+      leiningen
       maven
       neil
-    ]
-    # TODO these are here because of the custom versions in otm.nix
-    # but there should be a better way
-    ++ optionals stdenv.isLinux [ leiningen clojure ];
+    ];
 }
