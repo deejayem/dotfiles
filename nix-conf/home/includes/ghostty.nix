eb796be0b1e4956d7d0adec0ec88d41922c09788
David Morgan <djm_uk@protonmail.com>
2025-09-25 12:48:02 +0100

    Improve ghostty colours

--- nix-conf/home/includes/ghostty.nix
+++ nix-conf/home/includes/ghostty.nix
@@ -13,35 +13,34 @@ in
       font-family = "MesloLGS Nerd Font";
       font-size = if pkgs.stdenv.isDarwin then 12 else 8;
       copy-on-select = "clipboard";
-      # This is proposed syntax for the future, but ghostty is unusable on darwin until it's implemented
+      # This is proposed syntax for the future, but for now we achieve this with karabiner
       #key-remap = [ "ctrl=super" "super=ctrl" ];
       keybind = [
         "shift+insert=paste_from_clipboard"
       ];
 
       background = "#000000";
-      foreground = "#ffffff";
-      cursor-color = "#ffffff";
-      selection-background = "#c1ddff";
+      foreground = "#c7c7c7";
+      cursor-color = "#c7c7c7";
+      selection-background = "#b5d5ff";
       selection-foreground = "#000000";
-
       palette = [
-        "0=#2e3436"
-        "1=#cc0000"
-        "2=#4e9a06"
-        "3=#c4a000"
-        "4=#3465a4"
-        "5=#75507b"
-        "6=#06989a"
-        "7=#d3d7cf"
-        "8=#555753"
-        "9=#ef2929"
-        "10=#8ae234"
-        "11=#fce94f"
-        "12=#729fcf"
-        "13=#ad7fa8"
-        "14=#34e2e2"
-        "15=#eeeeec"
+        "0=#000000" # Black
+        "1=#c91b00" # Red
+        "2=#00c200" # Green
+        "3=#c7c400" # Yellow
+        "4=#0225c7" # Blue
+        "5=#ca30c7" # Magenta
+        "6=#00c5c7" # Cyan
+        "7=#c7c7c7" # White
+        "8=#686868" # Bright Black
+        "9=#ff6e67" # Bright Red
+        "10=#5ff967" # Bright Green
+        "11=#fefb67" # Bright Yellow
+        "12=#6871ff" # Bright Blue
+        "13=#ff76ff" # Bright Magenta
+        "14=#5ffdff" # Bright Cyan
+        "15=#feffff" # Bright White
       ];
     }
     // optionalAttrs pkgs.stdenv.isDarwin {
