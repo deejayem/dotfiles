7a80a24c3a8131d57c66b91fe25fc70bfee4a3d0
David Morgan <djm_uk@protonmail.com>
2025-10-04 17:30:37 +0100

    Try to use nix dependencies for msgpack-tools

--- nix-conf/pkgs/msgpack-tools/default.nix
+++ nix-conf/pkgs/msgpack-tools/default.nix
@@ -1,21 +1,15 @@
 {
   lib,
   stdenv,
-  fetchurl,
   fetchFromGitHub,
+  fetchurl,
   cmake,
+  rapidjson,
+  substitute,
+  libb64,
   versionCheckHook,
 }:
-
 let
-  libb64 = fetchurl {
-    url = "mirror://sourceforge/libb64/libb64-1.2.1.zip";
-    hash = "sha256-IBBvC6lc/Zw1oTxxIGZD4/s+RlEt8+Lvsv2/hxFjFLI=";
-  };
-  rapidjson = fetchurl {
-    url = "https://github.com/miloyip/rapidjson/archive/9bd618f545ab647e2c3bcbf2f1d87423d6edf800.tar.gz";
-    hash = "sha256-O66yxOidLgLqk5+PAuP67H8eDxnVOK+3BQCQPjrPVxM=";
-  };
   mpack = fetchurl {
     url = "https://github.com/ludocode/mpack/archive/df17e83f0fa8571b9cd0d8ccf38144fa90e244d1.tar.gz";
     hash = "sha256-hyiXygbAHnNgF4TIg+DemBvtdBnSgJ7fAhknVuL+T/c=";
@@ -29,24 +23,36 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "ludocode";
     repo = "msgpack-tools";
     rev = "v${finalAttrs.version}";
-    sha256 = "1ygjk25zlpqjckxgqmahnz999704zy2bd9id6hp5jych1szkjgs5";
+    hash = "sha256-RT85vw6QeVkuNC2mtoT/BJyU0rdQVfz6ZBJf+ouY8vk=";
   };
 
-  # fix rapidjson dependency version
-  # https://github.com/ludocode/msgpack-tools/issues/18
-  postPatch = ''
-    substituteInPlace CMakeLists.txt \
-       --replace-fail 'set(RAPIDJSON_COMMIT "99ba17bd66a85ec64a2f322b68c2b9c3b77a4391")' 'set(RAPIDJSON_COMMIT "9bd618f545ab647e2c3bcbf2f1d87423d6edf800")'
-  '';
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  buildInputs = [
+    rapidjson
+    libb64
+  ];
+
+  patches = [
+    (substitute {
+      src = ./use-nix-deps.patch;
+      substitutions = [
+        "--subst-var-by"
+        "rapidjson"
+        "${rapidjson}"
+        "--subst-var-by"
+        "libb64"
+        "${libb64}"
+      ];
+    })
+  ];
 
   postUnpack = ''
     mkdir $sourceRoot/contrib
-    cp ${rapidjson} $sourceRoot/contrib/rapidjson-9bd618f545ab647e2c3bcbf2f1d87423d6edf800.tar.gz
-    cp ${libb64} $sourceRoot/contrib/libb64-1.2.1.zip
     cp ${mpack} $sourceRoot/contrib/mpack-df17e83f0fa8571b9cd0d8ccf38144fa90e244d1.tar.gz
   '';
-
-  nativeBuildInputs = [ cmake ];
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgram = "${placeholder "out"}/bin/json2msgpack";
