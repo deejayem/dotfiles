12d03ac03a3fb2aa4ba6205ebbe98a24c88378ac
David Morgan <djm_uk@protonmail.com>
2025-10-08 14:10:25 +0100

    Tweak blue in foot again

--- nix-conf/home/includes/sway.nix
+++ nix-conf/home/includes/sway.nix
@@ -97,7 +97,7 @@
         regular1 = "cc0403"; # red
         regular2 = "19cb00"; # green
         regular3 = "cecb00"; # yellow
-        regular4 = "0050ff"; # blue
+        regular4 = "3366ff"; # TODO or "4080ff"; # blue
         regular5 = "cb1ed1"; # magenta
         regular6 = "0dcdcd"; # cyan
         regular7 = "e5e5e5"; # white
