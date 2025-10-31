39a25b10ba68ed58110ba4ddbb6fd846d7b3c812
David Morgan <djm_uk@protonmail.com>
2025-10-31 20:09:00 +0000

    Remove almost everything from linux-server.nix

--- nix-conf/hosts/home-manager/modules/linux-server.nix
+++ nix-conf/hosts/home-manager/modules/linux-server.nix
@@ -1,20 +1,4 @@
+{ ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  imports = [ ./common.nix ];
-
   host.role = "server";
-
-  home.packages = with pkgs; [
-    emacs-nox # will go in emacs.nix
-    libtree # common, when not darwin
-    msmtp # remove
-    restic # common
-    sword # common
-    yt-dlp # common
-  ];
 }
