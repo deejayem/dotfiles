19dc7db6f5805bcebfe0b0edf0ad4626d4a3e187
David Morgan <djm_uk@protonmail.com>
2025-10-31 14:13:41 +0000

    Improve overlay code

--- nix-conf/overlays/default.nix
+++ nix-conf/overlays/default.nix
@@ -1,7 +1,15 @@
 { inputs, ... }:
 let
+  ignoreList = [
+    "default\\.nix"
+    "flake-module\\.nix"
+    "_.*"
+  ];
+
   files = builtins.filter (
-    f: f != "default.nix" && f != "flake-module.nix" && builtins.match ".*\\.nix$" f != null
+    f:
+    !(builtins.any (pattern: builtins.match pattern f != null) ignoreList)
+    && builtins.match ".*\\.nix$" f != null
   ) (builtins.attrNames (builtins.readDir ./.));
 in
 # Import (almost) every overlays/foo.nix file as overlays.foo
