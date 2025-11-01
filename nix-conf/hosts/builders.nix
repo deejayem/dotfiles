cd673a0a24311b037d3f79b616f8d34a9896cc0d
David Morgan <djm_uk@protonmail.com>
2025-11-01 17:21:09 +0000

    Split up hosts/flake-module.nix

---
+++ nix-conf/hosts/builders.nix
@@ -0,0 +1,125 @@
+{ inputs, self }:
+let
+  nix-darwin = inputs.nix-darwin;
+
+  mkNixosConfig =
+    versions: hostname:
+    {
+      system,
+      version,
+      role,
+      ...
+    }:
+    versions.withVersions version (
+      { nixpkgs, ... }:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs role; };
+        modules = [
+          ../config.nix
+          ./nixos/${hostname}/configuration.nix
+        ];
+      }
+    );
+
+  mkDarwinConfig =
+    versions: hostname:
+    {
+      role,
+      ...
+    }:
+    nix-darwin.lib.darwinSystem {
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      specialArgs = { inherit inputs role; };
+      modules = [
+        ../config.nix
+        ./darwin/configuration.nix
+      ];
+    };
+
+  mkHomeConfig =
+    versions: hostname: username:
+    {
+      system,
+      version,
+      role,
+      ...
+    }:
+    versions.withVersions version (
+      { nixpkgs, home-manager }:
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        extraSpecialArgs = {
+          inherit
+            inputs
+            system
+            version
+            role
+            ;
+        };
+        modules = [
+          ../config.nix
+          ./home-manager/${hostname}.nix
+        ];
+      }
+    );
+
+  mapHosts =
+    f: hosts: builtins.concatMap (hostname: f hostname hosts.${hostname}) (builtins.attrNames hosts);
+
+  mkNixosConfigurations =
+    versions: hosts:
+    builtins.listToAttrs (
+      mapHosts (
+        hostname: cfg:
+        if cfg ? nixos then
+          [
+            {
+              name = hostname;
+              value = (mkNixosConfig versions) hostname cfg;
+            }
+          ]
+        else
+          [ ]
+      ) hosts
+    );
+
+  mkDarwinConfigurations =
+    versions: hosts:
+    builtins.listToAttrs (
+      mapHosts (
+        hostname: cfg:
+        if cfg ? darwin then
+          [
+            {
+              name = hostname;
+              value = (mkDarwinConfig versions) hostname cfg;
+            }
+          ]
+        else
+          [ ]
+      ) hosts
+    );
+
+  mkHomeConfigurations =
+    versions: hosts:
+    builtins.listToAttrs (
+      mapHosts (
+        hostname: cfg:
+        if cfg ? home then
+          builtins.map (username: {
+            name = "${username}@${hostname}";
+            value = (mkHomeConfig versions) hostname username cfg;
+          }) (builtins.attrNames cfg.home)
+        else
+          [ ]
+      ) hosts
+    );
+in
+{
+  mkConfigurations = versions: hosts: {
+    nixosConfigurations = mkNixosConfigurations versions hosts;
+    darwinConfigurations = mkDarwinConfigurations versions hosts;
+    homeConfigurations = mkHomeConfigurations versions hosts;
+  };
+}
---
+++ nix-conf/hosts/definitions.nix
@@ -0,0 +1,81 @@
+{ inputs }:
+let
+  roles = {
+    workstation = "workstation";
+    server = "server";
+  };
+
+  versions = rec {
+    stable = "stable";
+    unstable = "unstable";
+
+    config = {
+      ${stable} = {
+        nixpkgs = inputs.nixpkgs-stable;
+        home-manager = inputs.home-manager-stable;
+      };
+      ${unstable} = {
+        nixpkgs = inputs.nixpkgs-unstable;
+        home-manager = inputs.home-manager-unstable;
+      };
+    };
+
+    get =
+      v:
+      config.${v}
+        or (throw "Invalid version '${v}'. Must be one of: ${builtins.concatStringsSep ", " (builtins.attrNames config)}");
+
+    withVersions = version: f: f (get version);
+  };
+
+  systems = {
+    x86_64-linux = "x86_64-linux";
+    aarch64-linux = "aarch64-linux";
+    aarch64-darwin = "aarch64-darwin";
+  };
+in
+{
+  inherit roles versions systems;
+
+  hosts = {
+    egalmoth = {
+      system = systems.x86_64-linux;
+      version = versions.stable;
+      role = roles.workstation;
+      nixos = { };
+      home.djm = { };
+    };
+
+    edrahil = {
+      system = systems.x86_64-linux;
+      version = versions.stable;
+      role = roles.server;
+      nixos = { };
+      home.djm = { };
+    };
+
+    djmuk1 = {
+      system = systems.x86_64-linux;
+      version = versions.stable;
+      role = roles.server;
+      nixos = { };
+      home.djm = { };
+    };
+
+    djmuk2 = {
+      system = systems.aarch64-linux;
+      version = versions.stable;
+      role = roles.server;
+      nixos = { };
+      home.djm = { };
+    };
+
+    grithnir = {
+      system = systems.aarch64-darwin;
+      version = versions.unstable;
+      role = roles.workstation;
+      darwin = { };
+      home.djm = { };
+    };
+  };
+}
