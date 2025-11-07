246ae7cbee828ea2f8f4eca525f6bdb828c91724
David Morgan <djm_uk@protonmail.com>
2025-11-07 17:16:49 +0000

    Make djmuk2/private.nix just data

--- nix-conf/hosts/nixos/djmuk2/configuration.nix
+++ nix-conf/hosts/nixos/djmuk2/configuration.nix
@@ -1,14 +1,19 @@
 { ... }:
+let
+  private = import ./private.nix;
+in
 {
   imports = [
     ./hardware-configuration.nix
-    ./private.nix
     ../modules/base.nix
   ];
 
   networking.hostName = "djmuk2";
 
-  services.openiscsi.enable = true;
+  services.openiscsi = {
+    enable = true;
+    inherit (private.services.openiscsi) name;
+  };
 
   system.stateVersion = "22.05";
 }
