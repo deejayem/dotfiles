27a76b80f54ab0a4c7624d5c499bc8b9a4e3a888
David Morgan <djm_uk@protonmail.com>
2025-11-07 21:05:47 +0000

    Extract and refactor restic configuration

---
+++ nix-conf/hosts/nixos/edrahil/backups.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+let
+  commonConfig = {
+    paths = [ "${config.users.users.djm.home}" ];
+    initialize = true;
+    user = "djm";
+    environmentFile = "/etc/restic-environment";
+    passwordFile = config.sops.secrets.restic_password.path;
+    exclude = [
+      "irclogs"
+      ".cache"
+      ".config"
+      ".directory_history"
+      ".local"
+      "nixpkgs"
+    ];
+    extraBackupArgs = [ "--compression=max" ];
+    pruneOpts = [
+      "--keep-daily 5"
+      "--keep-weekly 2"
+      "--keep-monthly 3"
+    ];
+  };
+  mkBackup =
+    host: time:
+    commonConfig
+    // {
+      repository = "sftp:djm@${host}-backup:/home/djm/backup/edrahil";
+      timerConfig = {
+        OnCalendar = time;
+        RandomizedDelaySec = "20min";
+      };
+    };
+in
+{
+  sops.secrets.restic_password = {
+    owner = config.users.users.djm.name;
+  };
+
+  services.restic.backups = {
+    hb = mkBackup "hb" "02:25";
+    bs = mkBackup "bs" "03:15";
+    tt = mkBackup "tt" "04:05";
+  };
+
+  environment.etc = {
+    "restic-environment" = {
+      text = ''
+        RESTIC_COMPRESSION=max
+      '';
+    };
+  };
+}
--- nix-conf/hosts/nixos/edrahil/configuration.nix
+++ nix-conf/hosts/nixos/edrahil/configuration.nix
@@ -8,6 +8,7 @@ in
   imports = [
     ./hardware-configuration.nix
     ./network-configuration.nix
+    ./backups.nix
     ../modules/base.nix
     inputs.sops-nix.nixosModules.sops
   ];
@@ -19,111 +20,9 @@ in
   services.openssh.ports = [ 2222 ];
   services.openssh.allowSFTP = true;
 
-  sops = {
-    defaultSopsFile = builtins.path {
-      path = ./secrets.yaml;
-      name = "${config.networking.hostName}-secrets.yaml";
-    };
-    secrets.restic_password = {
-      owner = config.users.users.djm.name;
-    };
-  };
-
-  services.restic = {
-    backups = {
-      hb = {
-        paths = [ "${config.users.users.djm.home}" ];
-        repository = "sftp:djm@hb-backup:/home/djm/backup/edrahil";
-        initialize = true;
-        user = "djm";
-        environmentFile = "/etc/restic-environment";
-        passwordFile = config.sops.secrets.restic_password.path;
-        timerConfig = {
-          OnCalendar = "02:25";
-          RandomizedDelaySec = "20min";
-        };
-        exclude = [
-          "irclogs"
-          ".cache"
-          ".config"
-          ".directory_history"
-          ".local"
-          "nixpkgs"
-        ];
-        extraBackupArgs = [
-          "--compression=max"
-        ];
-        pruneOpts = [
-          "--keep-daily 5"
-          "--keep-weekly 2"
-          "--keep-monthly 3"
-        ];
-      };
-      bs = {
-        paths = [ "${config.users.users.djm.home}" ];
-        repository = "sftp:djm@bs-backup:/home/djm/backup/edrahil";
-        initialize = true;
-        user = "djm";
-        environmentFile = "/etc/restic-environment";
-        passwordFile = config.sops.secrets.restic_password.path;
-        timerConfig = {
-          OnCalendar = "03:15";
-          RandomizedDelaySec = "20min";
-        };
-        exclude = [
-          "irclogs"
-          ".cache"
-          ".config"
-          ".directory_history"
-          ".local"
-          "nixpkgs"
-        ];
-        extraBackupArgs = [
-          "--compression=max"
-        ];
-        pruneOpts = [
-          "--keep-daily 5"
-          "--keep-weekly 2"
-          "--keep-monthly 3"
-        ];
-      };
-      tt = {
-        paths = [ "${config.users.users.djm.home}" ];
-        repository = "sftp:djm@tt-backup:/home/djm/backup/edrahil";
-        initialize = true;
-        user = "djm";
-        environmentFile = "/etc/restic-environment";
-        passwordFile = config.sops.secrets.restic_password.path;
-        timerConfig = {
-          OnCalendar = "04:05";
-          RandomizedDelaySec = "20min";
-        };
-        exclude = [
-          "irclogs"
-          ".cache"
-          ".config"
-          ".directory_history"
-          ".local"
-          "nixpkgs"
-        ];
-        extraBackupArgs = [
-          "--compression=max"
-        ];
-        pruneOpts = [
-          "--keep-daily 5"
-          "--keep-weekly 2"
-          "--keep-monthly 3"
-        ];
-      };
-    };
-  };
-
-  environment.etc = {
-    "restic-environment" = {
-      text = ''
-        RESTIC_COMPRESSION=max
-      '';
-    };
+  sops.defaultSopsFile = builtins.path {
+    path = ./secrets.yaml;
+    name = "${config.networking.hostName}-secrets.yaml";
   };
 
   system.stateVersion = "22.05";
