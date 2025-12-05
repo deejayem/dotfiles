b5e5ca3fbb85e8ef2f436096023b78f94e86d68f
David Morgan <djm_uk@protonmail.com>
2025-12-05 21:41:30 +0000

    Stop boot messages from breaking tuigreet now we're forced onto vt1

--- nix-conf/hosts/nixos/modules/services/greeter.nix
+++ nix-conf/hosts/nixos/modules/services/greeter.nix
@@ -3,17 +3,30 @@
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = ''
-        ${lib.getExe pkgs.tuigreet} \
-          --time \
-          --asterisks \
-          --user-menu \
-          --cmd sway
-      '';
+      default_session = {
+        command = ''
+          ${lib.getExe pkgs.tuigreet} \
+            --time \
+            --asterisks \
+            --cmd sway
+        '';
+      };
     };
   };
 
   environment.etc."greetd/environments".text = ''
     sway
   '';
+
+  # Prevent boot messages from messing up tuigreet
+  # (https://github.com/sjcobb2022/nixos-config/blob/720ffb2c9c7f3643460de5f31bcca30ec38c1ebe/hosts/common/optional/greetd.nix)
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
 }
