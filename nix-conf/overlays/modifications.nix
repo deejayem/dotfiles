89f0192f245cafd9451dd9ce379d41699aa0ab58
David Morgan <djm_uk@protonmail.com>
2025-12-09 12:52:27 +0000

    Override orbstack version

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -1,4 +1,19 @@
 { ... }:
 final: prev: {
+  orbstack = prev.orbstack.overrideAttrs (
+    old:
+    let
+      version = "2.0.5-19905";
+    in
+    {
+      inherit version;
 
+      src = prev.fetchurl {
+        url = "https://cdn-updates.orbstack.dev/arm64/OrbStack_v${
+          prev.lib.replaceString "-" "_" version
+        }_arm64.dmg";
+        hash = "sha256-YBiJVRzf3H/u4Ui3/bBID6C6XA2wvo8cBH/IQIhqdXE=";
+      };
+    }
+  );
 }
