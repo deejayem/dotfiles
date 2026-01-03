df14dd2cc8034f8f0efa96c4722eb1084584d010
David Morgan <djm_uk@protonmail.com>
2026-01-03 17:16:05 +0000

    Remove zoom version override

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -51,36 +51,4 @@ in
       vendorHash = "sha256-jJSSXMyUqcJoZuqfSlBsKDQezyqS+jDkRglMMjG1K8g=";
     }
   );
-  zoom-us = prev.zoom-us.overrideAttrs (
-    _:
-    let
-      system = prev.stdenv.hostPlatform.system;
-
-      versions = {
-        aarch64-darwin = "6.7.0.71075";
-        x86_64-darwin = "6.7.0.71075";
-        x86_64-linux = "6.7.0.6313";
-      };
-
-      srcs = {
-        aarch64-darwin = prev.fetchurl {
-          url = "https://zoom.us/client/${versions.aarch64-darwin}/zoomusInstallerFull.pkg?archType=arm64";
-          name = "zoomusInstallerFull.pkg";
-          hash = "sha256-Jj6Ikxk7W77sv6g6yYR9ttTRF3kooQgVJnExNaU5aAA=";
-        };
-        x86_64-darwin = prev.fetchurl {
-          url = "https://zoom.us/client/${versions.x86_64-darwin}/zoomusInstallerFull.pkg";
-          hash = "sha256-OaF3cbJZcXKKPkvdfjMuL1Va944N/TOAlqCLdA1fl64=";
-        };
-        x86_64-linux = prev.fetchurl {
-          url = "https://zoom.us/client/${versions.x86_64-linux}/zoom_x86_64.pkg.tar.xz";
-          hash = "sha256-Bm5LEGmGN0iUwzKdVzicxfx6K3g9FGevvR/gUIBaPj8=";
-        };
-      };
-    in
-    {
-      version = versions.${system};
-      src = srcs.${system};
-    }
-  );
 }
