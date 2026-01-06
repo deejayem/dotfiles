ff62c7bdc273e21fb5e7cea315eb6e40e3e56d8c
David Morgan <djm_uk@protonmail.com>
2026-01-06 09:17:36 +0000

    Override zoom version (6.7.2.72191)

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -16,4 +16,36 @@ final: prev: {
       };
     }
   );
+  zoom-us = prev.zoom-us.overrideAttrs (
+    _:
+    let
+      system = prev.stdenv.hostPlatform.system;
+
+      versions = {
+        aarch64-darwin = "6.7.2.72191";
+        x86_64-darwin = "6.7.2.72191";
+        x86_64-linux = "6.7.2.6498";
+      };
+
+      srcs = {
+        aarch64-darwin = prev.fetchurl {
+          url = "https://zoom.us/client/${versions.aarch64-darwin}/zoomusInstallerFull.pkg?archType=arm64";
+          name = "zoomusInstallerFull.pkg";
+          hash = "sha256-v4maXehUE0KSmZO2OI/d2qVyeTnGvkaWaQzul/yde80=";
+        };
+        x86_64-darwin = prev.fetchurl {
+          url = "https://zoom.us/client/${versions.x86_64-darwin}/zoomusInstallerFull.pkg";
+          hash = "sha256-6hDDzxzZMisPPyrIQ6XIhipVbid4a/X2gaV4DYIfz3w=";
+        };
+        x86_64-linux = prev.fetchurl {
+          url = "https://zoom.us/client/${versions.x86_64-linux}/zoom_x86_64.pkg.tar.xz";
+          hash = "sha256-fvZMKjzQFkIKl3TUOMYHR75SDHShkKjk59ps55o9Qks=";
+        };
+      };
+    in
+    {
+      version = versions.${system};
+      src = srcs.${system};
+    }
+  );
 }
