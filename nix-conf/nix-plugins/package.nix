192d77427f004656e511afe646bff34b6f1ed138
David Morgan <djm_uk@protonmail.com>
2026-01-07 13:39:14 +0000

    Always use stable nixComponents for nix-plugins

--- nix-conf/nix-plugins/package.nix
+++ nix-conf/nix-plugins/package.nix
@@ -1,10 +1,17 @@
 { pkgs }:
+let
+  inherit (pkgs) lib;
+  nix-version = lib.replaceStrings [ "." ] [ "_" ] (
+    lib.versions.majorMinor pkgs.nixVersions.stable.version
+  );
+in
 (pkgs.nix-plugins.override {
-  nixComponents = pkgs.nixVersions.nixComponents_2_31;
-}).overrideAttrs (old: {
-  # Workaround: include extra-builtins.nix in nix-plugins package; see comment in ./default.nix
-  postInstall = (old.postInstall or "") + ''
-    mkdir -p $out/share/nix-plugins
-    cp ${./extra-builtins.nix} $out/share/nix-plugins/extra-builtins.nix
-  '';
-})
+  nixComponents = pkgs.nixVersions."nixComponents_${nix-version}";
+}).overrideAttrs
+  (old: {
+    # Workaround: include extra-builtins.nix in nix-plugins package; see comment in ./default.nix
+    postInstall = (old.postInstall or "") + ''
+      mkdir -p $out/share/nix-plugins
+      cp ${./extra-builtins.nix} $out/share/nix-plugins/extra-builtins.nix
+    '';
+  })
