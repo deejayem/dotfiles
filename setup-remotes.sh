615df4b23d698959b103541cc4579fabbe358c1d
David Morgan <djm_uk@protonmail.com>
2026-01-08 12:25:27 +0000

    Improve whitespace

--- setup-remotes.sh
+++ setup-remotes.sh
@@ -6,5 +6,5 @@ git remote add tildegit git@tildegit.org:djm/dotfiles.git
 git remote add tilde.institute djm@tilde.institute:public_repos/dotfiles
 git remote add github git@github.com:deejayem/dotfiles.git
 
-for i in `git remote -v | grep -v origin|awk '{print $2}' | uniq`; do git remote set-url --add --push origin $i ; done
+for i in `git remote -v | grep -v origin | awk '{print $2}' | uniq`; do git remote set-url --add --push origin $i ; done
 
