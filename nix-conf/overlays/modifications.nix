4482bc92ad49e0872fca8ee9d2e47584d0f64aea
David Morgan <djm_uk@protonmail.com>
2026-01-12 13:54:31 +0000

    Pin cdktf-cli nodejs version to 22

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -1,5 +1,14 @@
 { ... }:
 final: prev: {
+  cdktf-cli = prev.cdktf-cli.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = builtins.map (pkg:
+      if pkg == prev.nodejs then prev.nodejs_22 else pkg
+    ) oldAttrs.nativeBuildInputs;
+    installPhase = builtins.replaceStrings
+      [ "${prev.lib.getExe prev.nodejs}" ]
+      [ "${prev.lib.getExe prev.nodejs_22}" ]
+      oldAttrs.installPhase;
+  });
   orbstack = prev.orbstack.overrideAttrs (
     _:
     let
