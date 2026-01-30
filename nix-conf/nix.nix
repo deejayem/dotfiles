0518e6ebc079d5ee9b39232cc041cb5c048afa05
David Morgan <djm_uk@protonmail.com>
2026-01-30 08:00:23 +0000

    Add nix-community.cachix.org to extra-substituters

--- nix-conf/nix.nix
+++ nix-conf/nix.nix
@@ -29,6 +29,12 @@
     package = pkgs.nix;
     settings = {
       experimental-features = "nix-command flakes";
+      extra-substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 }
