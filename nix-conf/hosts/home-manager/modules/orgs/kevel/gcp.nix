b0c168b02d3ed0e7866b955696d5ec75faff573b
David Morgan <djm_uk@protonmail.com>
2026-02-05 13:33:31 +0000

    Add gcp project switch function

--- nix-conf/hosts/home-manager/modules/orgs/kevel/gcp.nix
+++ nix-conf/hosts/home-manager/modules/orgs/kevel/gcp.nix
@@ -33,61 +33,69 @@ in
     };
   };
 
-  programs.zsh.initContent = ''
-    source "${pkgs.google-cloud-sdk}/share/zsh/site-functions/_gcloud"
+  programs.zsh = {
+    initContent = ''
+      source "${pkgs.google-cloud-sdk}/share/zsh/site-functions/_gcloud"
 
-    # Prevent spaces from being escaped
-    # e.g. gcloud compute ssh foo<tab> becomes gcloud compute ssh foo-bar\ --zone=us-east1-a
-    _complete_gcloud() {
-      local -x COMP_POINT COMP_CWORD
-      local -a COMP_WORDS COMPREPLY
-      local -x COMP_LINE="$words"
+      # Prevent spaces from being escaped
+      # e.g. gcloud compute ssh foo<tab> becomes gcloud compute ssh foo-bar\ --zone=us-east1-a
+      _complete_gcloud() {
+        local -x COMP_POINT COMP_CWORD
+        local -a COMP_WORDS COMPREPLY
+        local -x COMP_LINE="$words"
 
-      (( COMP_POINT = ''${#COMP_LINE} ))
-      (( COMP_CWORD = CURRENT - 1 ))
-      COMP_WORDS=( $words )
+        (( COMP_POINT = ''${#COMP_LINE} ))
+        (( COMP_CWORD = CURRENT - 1 ))
+        COMP_WORDS=( $words )
 
-      _python_argcomplete ${gcloud}
+        _python_argcomplete ${gcloud}
 
-      local -a fixed=()
-      local m
-      for m in "''${COMPREPLY[@]}"; do
-          fixed+=("''${m//\\/}")
-      done
+        local -a fixed=()
+        local m
+        for m in "''${COMPREPLY[@]}"; do
+            fixed+=("''${m//\\/}")
+        done
 
-      compadd -Q -S ' ' -- "''${fixed[@]}"
-    }
+        compadd -Q -S ' ' -- "''${fixed[@]}"
+      }
 
-    compdef _complete_gcloud gcloud
+      compdef _complete_gcloud gcloud
 
-    # ssh completion for gcp hosts
-    _gcp_ssh() {
-      local prefix project cache_file
-      local -a instances config_hosts
+      # ssh completion for gcp hosts
+      _gcp_ssh() {
+        local prefix project cache_file
+        local -a instances config_hosts
 
-      case "$words[CURRENT]" in
-          gcp1-*) prefix=gcp1; project="$GCP_DELIVERY_DEV" ;;
-          pg1-*)  prefix=pg1;  project="$GCP_DELIVERY_PROD" ;;
-          *)      _ssh "$@"; return ;;
-      esac
+        case "$words[CURRENT]" in
+            gcp1-*) prefix=gcp1; project="$GCP_DELIVERY_DEV" ;;
+            pg1-*)  prefix=pg1;  project="$GCP_DELIVERY_PROD" ;;
+            *)      _ssh "$@"; return ;;
+        esac
 
-      [[ -z "$project" ]] && { _ssh "$@"; return }
+        [[ -z "$project" ]] && { _ssh "$@"; return }
 
-      cache_file="''${XDG_CACHE_HOME:-$HOME/.cache}/gcp-instances-$prefix"
+        cache_file="''${XDG_CACHE_HOME:-$HOME/.cache}/gcp-instances-$prefix"
 
-      if [[ ! -f "$cache_file" ]]; then
-          ${gcloud} compute instances list --project="$project" --format='value(name)' 2>/dev/null > "$cache_file"
-      elif [[ $(( $(date +%s) - $(stat -f%m "$cache_file" 2>/dev/null || stat -c%Y "$cache_file") )) -gt 300 ]]; then
-          { ${gcloud} compute instances list --project="$project" --format='value(name)' 2>/dev/null > "$cache_file" } &!
-      fi
+        if [[ ! -f "$cache_file" ]]; then
+            ${gcloud} compute instances list --project="$project" --format='value(name)' 2>/dev/null > "$cache_file"
+        elif [[ $(( $(date +%s) - $(stat -f%m "$cache_file" 2>/dev/null || stat -c%Y "$cache_file") )) -gt 300 ]]; then
+            { ${gcloud} compute instances list --project="$project" --format='value(name)' 2>/dev/null > "$cache_file" } &!
+        fi
 
-      instances=(''${(f)"$(<$cache_file)"})
+        instances=(''${(f)"$(<$cache_file)"})
 
-      config_hosts=(''${(f)"$(${grep} -h '^Host ' ~/.ssh/config ~/.ssh/config.local ~/.ssh/config.d/* 2>/dev/null | ${awk} '{print $2}' | ${grep} "^''${prefix}-")"})
+        config_hosts=(''${(f)"$(${grep} -h '^Host ' ~/.ssh/config ~/.ssh/config.local ~/.ssh/config.d/* 2>/dev/null | ${awk} '{print $2}' | ${grep} "^''${prefix}-")"})
 
-      compadd -Q -- "''${instances[@]}" "''${config_hosts[@]}"
-    }
+        compadd -Q -- "''${instances[@]}" "''${config_hosts[@]}"
+      }
 
-    compdef _gcp_ssh ssh
-  '';
+      compdef _gcp_ssh ssh
+    '';
+
+    siteFunctions = {
+      gcs = ''
+        ${gcloud} config set project "$1" && ${gcloud} auth application-default set-quota-project "$1"
+      '';
+    };
+  };
 }
