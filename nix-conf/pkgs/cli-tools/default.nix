90c8f0ea9350d56d8304c263b6d6215f75144b87
David Morgan <djm_uk@protonmail.com>
2026-02-09 12:25:34 +0000

    Update cli-tools

--- nix-conf/pkgs/cli-tools/default.nix
+++ nix-conf/pkgs/cli-tools/default.nix
@@ -11,8 +11,8 @@ stdenvNoCC.mkDerivation {
   src = fetchFromPrivateGitHub {
     owner = "adzerk";
     repo = "cli-tools";
-    rev = "8689c89122c9a382d5f1d78020c82aa36ccc4944";
-    narHash = "sha256-py3I6hkhF7Bm/TOuCLAI9k1/mV9MPerNB2ENmQDNUpQ=";
+    rev = "29ec2846b5ad3a42db06cfd8ebd437b5602e68c2";
+    narHash = "sha256-CrkBRTors5d8l8AVSlwAfkqcmKDIdyhS60w+c+Twvbc=";
   };
 
   installPhase = ''
