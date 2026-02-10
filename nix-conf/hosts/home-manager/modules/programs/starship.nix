d1fe2cba6817edecb3e20c1212e8c2d7d5570067
David Morgan <djm_uk@protonmail.com>
2026-02-10 17:10:07 +0000

    Improve handling of starship modules in prompt

--- nix-conf/hosts/home-manager/modules/programs/starship.nix
+++ nix-conf/hosts/home-manager/modules/programs/starship.nix
@@ -1,13 +1,132 @@
-{ ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  comm = lib.getExe' pkgs.coreutils "comm";
+  grep = lib.getExe pkgs.gnugrep;
+  sort = lib.getExe' pkgs.coreutils "sort";
+  starship = lib.getExe pkgs.starship;
+in
 {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     settings = {
       add_newline = false;
-      format = ''
-        $os$username$hostname$localip$shlvl$singularity$kubernetes$nats$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$bun$c$cmake$cobol$cpp$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$gleam$golang$gradle$haskell$haxe$helm$java$julia$kotlin$lua$mojo$nim$nodejs$ocaml$odin$opa$perl$php$pulumi$purescript$python$quarto$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$guix_shell$nix_shell$conda$pixi$meson$spack$memory_usage$openstack$azure$direnv$env_var$mise$crystal$custom$sudo$jobs$battery$time$container$netns$shell$fill$status$cmd_duration$aws$gcloud$line_break$character
-      '';
+      format = lib.concatStrings [
+        # Identity & location
+        "$os"
+        "$username"
+        "$hostname"
+        "$localip"
+        "$shlvl"
+        # Containers
+        "$singularity"
+        "$kubernetes"
+        "$nats"
+        # Directory & VCS
+        "$directory"
+        "$vcsh"
+        "$fossil_branch"
+        "$fossil_metrics"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_metrics"
+        "$git_status"
+        "$hg_branch"
+        "$hg_state"
+        "$pijul_channel"
+        # Environment
+        "$docker_context"
+        "$package"
+        # Languages
+        "$bun"
+        "$c"
+        "$cmake"
+        "$cobol"
+        "$cpp"
+        "$daml"
+        "$dart"
+        "$deno"
+        "$dotnet"
+        "$elixir"
+        "$elm"
+        "$erlang"
+        "$fennel"
+        "$fortran"
+        "$gleam"
+        "$golang"
+        "$gradle"
+        "$haskell"
+        "$haxe"
+        "$helm"
+        "$java"
+        "$julia"
+        "$kotlin"
+        "$lua"
+        "$mojo"
+        "$nim"
+        "$nodejs"
+        "$ocaml"
+        "$odin"
+        "$opa"
+        "$perl"
+        "$php"
+        "$pulumi"
+        "$purescript"
+        "$python"
+        "$quarto"
+        "$raku"
+        "$rlang"
+        "$red"
+        "$ruby"
+        "$rust"
+        "$scala"
+        "$solidity"
+        "$swift"
+        "$terraform"
+        "$typst"
+        "$vlang"
+        "$vagrant"
+        "$xmake"
+        "$zig"
+        # Build systems & shells
+        "$buf"
+        "$guix_shell"
+        "$nix_shell"
+        "$conda"
+        "$pixi"
+        "$meson"
+        "$spack"
+        # System
+        "$memory_usage"
+        "$openstack"
+        "$azure"
+        "$direnv"
+        "$env_var"
+        "$mise"
+        "$crystal"
+        "$custom"
+        "$sudo"
+        "$jobs"
+        "$battery"
+        "$time"
+        "$container"
+        "$netns"
+        "$shell"
+        # Right-aligned
+        "$fill"
+        "$status"
+        "$cmd_duration"
+        "$aws"
+        "$gcloud"
+        # Input line
+        "$line_break"
+        "$character"
+      ];
       fill.symbol = " ";
       aws = {
         format = ''[$symbol($profile )(\($region\) )(\[$duration\] )]($style)'';
@@ -41,7 +160,7 @@
         symbol = " ";
       };
       git_status = {
-        format = ''([$all_status$ahead_behind]($style))'';
+        format = "([$all_status$ahead_behind]($style))";
         stashed = "[\\\$\${count} ](fg:#5fd700)"; # xterm 76 (chartreuse3)
         ahead = "[⇡\${count} ](fg:#87d700)"; # xterm 118 (chartreuse2)
         behind = "[⇣\${count} ](fg:#ff5f5f)"; # xterm 203 (indianred1)
@@ -89,5 +208,23 @@
         style = "bright-black"; # TODO is "white dimmed" better?
       };
     };
+  };
+
+  programs.zsh.siteFunctions = {
+    # Find differences between our version and the default config
+    starship-diff-modules = ''
+      local default_config local_config
+      default_config=$(STARSHIP_CONFIG=/dev/null ${starship} print-config 2>/dev/null \
+        | ${grep} '\$all is shorthand' | ${grep} -oE '\$[a-z_]+' | ${grep} -v '^\$all$' | ${sort})
+      local_config=$(${starship} print-config 2>/dev/null \
+        | ${grep} -m1 '^format' | ${grep} -oE '\$[a-z_]+' \
+        | ${grep} -v '^\$fill$' | ${sort})
+
+      echo "Missing from local config (in upstream default):"
+      ${comm} -23 <(<<< "$default_config") <(<<< "$local_config")
+      echo ""
+      echo "Extra in local config (not in upstream default):"
+      ${comm} -13 <(<<< "$default_config") <(<<< "$local_config")
+    '';
   };
 }
