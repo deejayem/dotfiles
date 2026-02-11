ad2f2d19b2d2ad00f5e57f74cb3cc603d2ba51da
David Morgan <djm_uk@protonmail.com>
2026-02-11 17:23:47 +0000

    Remove unnecessary pkgs in overlay setup

--- nix-conf/overlays/unstable.nix
+++ nix-conf/overlays/unstable.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 final: _prev: {
   unstable = import inputs.nixpkgs-unstable {
-    system = final.pkgs.stdenv.hostPlatform.system;
+    system = final.stdenv.hostPlatform.system;
   };
 }
