6d498e5c564420a49b0dafcb7de9af217a39fa73
David Morgan <djm_uk@protonmail.com>
2026-02-12 12:55:12 +0000

    Workround per-directory-history + atuin issue

--- nix-conf/hosts/home-manager/modules/programs/atuin.nix
+++ nix-conf/hosts/home-manager/modules/programs/atuin.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.atuin = {
     enable = true;
@@ -9,7 +9,18 @@
     ];
   };
 
-  programs.zsh.initContent = ''
+  programs.zsh.initContent = lib.mkAfter ''
+    # per-hirectory-history is causing preexec to pass the command as $2
+    # instead of $1, leaving $1 empty
+    if (( ''${+functions[_atuin_preexec]} )); then
+      functions[_atuin_preexec_orig]=$functions[_atuin_preexec]
+      _atuin_preexec() {
+        local cmd="$1"
+        [[ -z "$cmd" ]] && cmd="$2"
+        _atuin_preexec_orig "$cmd"
+      }
+    fi
+
     bindkey '^[r' atuin-search
   '';
 }
