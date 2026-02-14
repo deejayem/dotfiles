3bdc615376030b0c6db19a543e9dd72638e89592
David Morgan <djm_uk@protonmail.com>
2026-02-14 17:35:13 +0000

    Try more atuin settings

--- nix-conf/hosts/home-manager/modules/programs/atuin.nix
+++ nix-conf/hosts/home-manager/modules/programs/atuin.nix
@@ -10,11 +10,16 @@
     forceOverwriteSettings = true;
     settings = {
       dialect = "uk";
+      filter_mode_shell_up_key_binding = "session-preload";
+      inline_height = 30;
+      search_mode_shell_up_key_binding = "fulltext";
+      style = "full";
       update_check = false;
     };
   };
 
   programs.zsh.initContent = lib.mkAfter ''
     bindkey '^[r' atuin-search
+    bindkey '^[p' atuin-up-search
   '';
 }
