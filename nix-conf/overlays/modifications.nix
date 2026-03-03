99bd3588c212d9d86960b64ec8bcf4ccde7a645c
David Morgan <djm_uk@protonmail.com>
2026-03-03 12:20:20 +0000

    Remove cdktf override

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -6,15 +6,6 @@ final: prev: {
     ];
   });
 
-  cdktf-cli = prev.cdktf-cli.overrideAttrs (oldAttrs: {
-    nativeBuildInputs = builtins.map (
-      pkg: if pkg == prev.nodejs then prev.nodejs_22 else pkg
-    ) oldAttrs.nativeBuildInputs;
-    installPhase =
-      builtins.replaceStrings [ "${prev.lib.getExe prev.nodejs}" ] [ "${prev.lib.getExe prev.nodejs_22}" ]
-        oldAttrs.installPhase;
-  });
-
   firebase-tools = prev.firebase-tools.override {
     buildNpmPackage = prev.buildNpmPackage.override {
       nodejs = prev.nodejs_22;
