6a9655b37f0849faee844c8e6e692d7f7e83ca28
David Morgan <djm_uk@protonmail.com>
2026-03-06 17:23:05 +0000

    Improve output of check-versions script

--- nix-conf/pkgs/scripts/check-versions.nix
+++ nix-conf/pkgs/scripts/check-versions.nix
@@ -14,8 +14,24 @@ pkgs.writeShellScriptBin "check-versions" ''
 
   flake_dir="''${1:-.}"
 
+  green=$'\033[32m'
+  red=$'\033[31m'
+  bold=$'\033[1m'
+  reset=$'\033[0m'
+
   nix_ver() {
     ${nix} eval --raw "$1" 2>/dev/null
+  }
+
+  color_ver() {
+    local ver="$1" latest="$2" width="$3"
+    local color
+    if [[ "$ver" == "$latest" ]]; then
+      color="$green"
+    else
+      color="$red"
+    fi
+    printf '%b%-*s%b' "$color" "$width" "$ver" "$reset"
   }
 
   latest_chrome=$(${curl} -s 'https://versionhistory.googleapis.com/v1/chrome/platforms/mac_arm64/channels/stable/versions/all/releases?filter=endtime=none,fraction%3E=0.5&order_by=version%20desc' | ${jq} -r '.releases[0].version')
@@ -33,11 +49,20 @@ pkgs.writeShellScriptBin "check-versions" ''
   local_slack=$(nix_ver "''${flake_dir}#modifications.slack.version")
   local_zoom=$(nix_ver "''${flake_dir}#modifications.zoom-us.version")
 
-  {
-    echo "PACKAGE NIXPKGS LOCAL LATEST "
-    echo "chrome $nixpkgs_chrome $local_chrome $latest_chrome"
-    echo "firefox $nixpkgs_firefox $local_firefox $latest_firefox"
-    echo "slack $nixpkgs_slack $local_slack $latest_slack"
-    echo "zoom $nixpkgs_zoom $local_zoom $latest_zoom"
-  } | ${column} -t
+  w=18
+
+  row() {
+    local name="$1" nixpkgs="$2" local="$3" latest="$4"
+    printf '%-10s ' "$name"
+    color_ver "$nixpkgs" "$latest" "$w"
+    printf ' '
+    color_ver "$local" "$latest" "$w"
+    printf ' %s\n' "$latest"
+  }
+
+  printf "''${bold}%-10s %-''${w}s %-''${w}s %s''${reset}\n" PACKAGE NIXPKGS LOCAL LATEST
+  row chrome  "$nixpkgs_chrome"  "$local_chrome"  "$latest_chrome"
+  row firefox "$nixpkgs_firefox" "$local_firefox" "$latest_firefox"
+  row slack   "$nixpkgs_slack"   "$local_slack"   "$latest_slack"
+  row zoom    "$nixpkgs_zoom"    "$local_zoom"    "$latest_zoom"
 ''
