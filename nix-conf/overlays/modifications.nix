7ff5894f75a86dd4f11c3ca2f3b410e1b47171da
David Morgan <djm_uk@protonmail.com>
2026-03-17 14:58:27 +0000

    Remove browser version overrides

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -8,25 +8,6 @@ final: prev:
   });
 }
 // prev.lib.optionalAttrs (prev.stdenv.isDarwin && prev.stdenv.isAarch64) {
-  brave = prev.brave.overrideAttrs (
-    finalAttrs: _: {
-      version = "1.88.127";
-      src = prev.fetchurl {
-        url = "https://github.com/brave/brave-browser/releases/download/v${finalAttrs.version}/brave-v${finalAttrs.version}-darwin-arm64.zip";
-        hash = "sha256-GNz1O145cKSSWVMl02PtpXHQjtDblxyAnYFqhopd8qQ=";
-      };
-    }
-  );
-  google-chrome = prev.google-chrome.overrideAttrs (
-    finalAttrs: _: {
-      version = "146.0.7680.76";
-      slug = "adgzhtm53eqdw4h4wn64ebox7o6q";
-      src = prev.fetchurl {
-        url = "http://dl.google.com/release2/chrome/${finalAttrs.slug}_${finalAttrs.version}/GoogleChrome-${finalAttrs.version}.dmg";
-        hash = "sha256-nC8y6992wlx6DcN48glkeoZFSze1vNkbsqENmqC5nrQ=";
-      };
-    }
-  );
   slack = prev.slack.overrideAttrs (
     finalAttrs: _: {
       version = "4.48.100";
@@ -48,33 +29,6 @@ final: prev:
   );
 }
 // prev.lib.optionalAttrs prev.stdenv.isDarwin {
-  firefox-unwrapped =
-    let
-      version = "148.0.2";
-    in
-    import (prev.path + "/pkgs/applications/networking/browsers/firefox/packages/firefox.nix") {
-      inherit (final)
-        stdenv
-        lib
-        callPackage
-        fetchurl
-        nixosTests
-        ;
-      buildMozillaMach =
-        args:
-        final.buildMozillaMach (
-          args
-          // {
-            inherit version;
-            packageVersion = version;
-            src = prev.fetchurl {
-              url = "mirror://mozilla/firefox/releases/${version}/source/firefox-${version}.source.tar.xz";
-              sha512 = "3phncmlfr8pclmxxi83vqzsrdnbgwdfxw4929lg337l3apsvrci83igp8cvjbr02357qyfvy773ppfsmmnj9djn5x7p0f2z4gjkmaan";
-            };
-          }
-        );
-    };
-  firefox = final.wrapFirefox final.firefox-unwrapped { };
   haskellPackages = prev.haskellPackages.override {
     overrides = hfinal: hprev: {
       warp = prev.haskell.lib.dontCheck hprev.warp;
