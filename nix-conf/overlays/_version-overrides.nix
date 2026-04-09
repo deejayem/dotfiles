0f9abb92930ec158a0c6886289b5dacc5f6407fe
David Morgan <djm_uk@protonmail.com>
2026-04-09 17:13:18 +0100

    Update package versions

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -2,16 +2,16 @@
   brave.version = "1.88.138";
   brave.hash = "sha256-yDBGo/J2b5iu9e4NvXN3EgYEDYaMq1V9MVb9xeGHpHo=";
 
-  google-chrome.version = "146.0.7680.178";
-  google-chrome.slug = "aco4gwkxcefkxklazdyhgwkrndkq";
-  google-chrome.hash = "sha256-aGqEFAzQZLy85hbsjhgYr5eFYgCaMhOiUG00wSlANHk=";
+  google-chrome.version = "147.0.7727.56";
+  google-chrome.slug = "nrtg4j5s3af3rvfw2czxfvmywu";
+  google-chrome.hash = "sha256-IPeukB7JCeHOWr0Ma7YFfHWO58Py2/x/o2Ajz0Rc4oM=";
 
-  firefox.version = "149.0";
-  firefox.sha512 = "0d3g59b04xnrjmc75zq53d4csdwj1vys7nfc8b8srcvn1k3fig6g6f14pxcpb823682i6sc9cc49gyifi49rs03ly2hvdwgffkp3n6d";
+  firefox.version = "149.0.2";
+  firefox.sha512 = "37z7mv7gfxiczxjl08vqa6igvm50g7nfaa1bkrlx9lc0mxd51kgq8m0s21s7qnmh2r2rsjg252ljgxdayh8kl17f9mwdkcf0vp4cjl4";
 
   slack.version = "4.49.81";
   slack.hash = "sha256-V5t1i4GmOrEdIU8TtlCRS2QYitUkoh02YaNzisL+yAs=";
 
-  zoom-us.version = "6.7.7.76486";
-  zoom-us.hash = "sha256-Wl8nghOfwGYxwzVjCScMeuiowhKLPYV04cagOpnzGUs=";
+  zoom-us.version = "7.0.0.77593";
+  zoom-us.hash = "sha256-YSUaM8YAJHigm4M9W34/bD164M8f/hbhtcmHyUwFN20=";
 }
