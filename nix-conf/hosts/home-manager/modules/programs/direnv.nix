a1e0ed427c405a37f542af1e810bfe907287117b
David Morgan <djm_uk@protonmail.com>
2026-04-14 14:04:34 +0100

    Add support for centralised per-repository flakess

--- nix-conf/hosts/home-manager/modules/programs/direnv.nix
+++ nix-conf/hosts/home-manager/modules/programs/direnv.nix
@@ -1,7 +1,32 @@
-{ ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  git = lib.getExe pkgs.git;
+in
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
   };
+
+  home.file."src/.envrc".text = ''
+    use_repo_flake() {
+      local workdir root repo flake
+
+      workdir="''${DIRENV_WORKDIR:-$PWD}"
+      root="$(${git} -C "$workdir" rev-parse --show-toplevel 2>/dev/null || printf '%s\n' "$workdir")"
+      repo="$(basename "$root")"
+      flake="${config.home.homeDirectory}/dotfiles/nix-conf/repos/$repo"
+
+      [[ -e "$flake/flake.nix" ]] || return 0
+
+      use flake "$flake"
+    }
+
+    use_repo_flake
+  '';
 }
