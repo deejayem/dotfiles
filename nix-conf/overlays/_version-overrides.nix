df210f947d74623134ca43a0b216e95895c01049
David Morgan <djm_uk@protonmail.com>
2026-04-21 07:56:45 +0100

    Update browser versions

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -1,13 +1,13 @@
 {
-  brave.version = "1.89.132";
-  brave.hash = "sha256-3f29ehqJNQjYJ+vh15ZI2UJEB1VdBfLL3VWv8CRasCw=";
+  brave.version = "1.89.137";
+  brave.hash = "sha256-Ffc9se0j9ULZsZQktWzrUgBiLyC5QR1jAPg6IcHoOTI=";
 
-  google-chrome.version = "147.0.7727.56";
-  google-chrome.slug = "nrtg4j5s3af3rvfw2czxfvmywu";
-  google-chrome.hash = "sha256-IPeukB7JCeHOWr0Ma7YFfHWO58Py2/x/o2Ajz0Rc4oM=";
+  google-chrome.version = "147.0.7727.102";
+  google-chrome.slug = "adh7lit4ohn55bd2cyoftxkjiixa";
+  google-chrome.hash = "sha256-eslLqiI5DtExI1Hq31wYBNQGu4LktzeW/7Z+R/kPGzo=";
 
-  firefox.version = "149.0.2";
-  firefox.sha512 = "37z7mv7gfxiczxjl08vqa6igvm50g7nfaa1bkrlx9lc0mxd51kgq8m0s21s7qnmh2r2rsjg252ljgxdayh8kl17f9mwdkcf0vp4cjl4";
+  firefox.version = "150.0";
+  firefox.sha512 = "1qlw6yby3i0l6q9j03h589h9hj04afv3hp1v5kql9b7vz4pz80hgdd0gcz5jgpdv49jrrj975hycwxybwxnvspx6cww65vvdvlfms3p";
 
   slack.version = "4.49.81";
   slack.hash = "sha256-V5t1i4GmOrEdIU8TtlCRS2QYitUkoh02YaNzisL+yAs=";
