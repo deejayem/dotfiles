85a393765b67c7e95bf10e8ad29677b0024d6849
David Morgan <djm_uk@protonmail.com>
2026-05-22 13:22:13 +0100

    Remove version overrides

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -4,16 +4,9 @@
     hash = "sha256-u3KmZffPQpHzS9IxZ7UsL7D6ETGJxExil20vmD6flMo=";
   };
 
-  google-chrome = {
-    version = "148.0.7778.179";
-    slug = "adxxii2zvsza6zjfnjbfh6fn4tqq";
-    hash = "sha256-QBHyF222wnaEmI79CQFOXQl5WkRNwneCYd/JFNMEEWU=";
-  };
+  google-chrome = null;
 
-  firefox = {
-    version = "151.0.1";
-    sha512 = "1vp5iqd8dfp6r3pqy031rzipxm2w412kjxnffm0hlja6ab3q3anix68d0kvq0jynhbj1g1a1nns9qqq1s5x32wkacqkff3bjnxs34l4";
-  };
+  firefox = null;
 
   orbstack = null;
 
