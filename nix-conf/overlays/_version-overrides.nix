c87a42c3d8eecbd7b78c0707e5b4e8bc4773df78
David Morgan <djm_uk@protonmail.com>
2026-07-09 07:38:14 +0100

    Update browser versions

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -1,16 +1,19 @@
 {
   brave = {
-    version = "1.92.134";
-    hash = "sha256-K1p5mAYfEDJNZFSmoOQV7kHNsA5RQuyUl3T2KVsNcbw=";
+    version = "1.92.138";
+    hash = "sha256-REoAys3S8VfJSAWFvqk6L2d3qxAy2/ylhTx4/gsoTVQ=";
   };
 
   google-chrome = {
-    version = "150.0.7871.47";
-    slug = "ad2pojsfcj6hr5telal5kk4t6gua";
-    hash = "sha256-1VTMVSBLfSRp9CxPaRkmdgAC0H5KNuZNWywTGLSphUI=";
+    version = "150.0.7871.115";
+    slug = "acd57hhfrcygivr2dnxpq2q56zka";
+    hash = "sha256-AB16I7Tl/E4F4ydXHSLeyoKAWNwP7JSL0wLyrUvn3FE=";
   };
 
-  firefox = null;
+  firefox = {
+    version = "152.0.5";
+    sha512 = "3rc8k16dp8lzygpgcs8hiji0fqv4fdn9hj52ss8gpcipvvgk36m07klwl4xj0hycpx38an86kravjv3a2ilrpv6507l78x651zxrwkc";
+  };
 
   orbstack = null;
 
