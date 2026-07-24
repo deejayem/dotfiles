ee303c00897c244f0976b237495a95415a91a27c
David Morgan <djm_uk@protonmail.com>
2026-07-24 17:19:11 +0100

    Update version overrides

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -1,13 +1,13 @@
 {
   brave = {
-    version = "1.92.140";
-    hash = "sha256-g7R8Sz3apcIMrQX6xqWiz0MMDru4hQQj5Uauq59BE/o=";
+    version = "1.92.143";
+    hash = "sha256-EvfZgO8FAijof1Ml6gqSOyRndL8KYFdT0MNmVmuxAnU=";
   };
 
   google-chrome = {
-    version = "150.0.7871.129";
-    slug = "ggb3e3myl2poiiaqd2bbvqlrqa";
-    hash = "sha256-ym9rF6yrGMSibQDM4gKlAbOsIHnV1tPxyNq9KNLnR0I=";
+    version = "150.0.7871.187";
+    slug = "ecziwrw2etq4lm3vlbpv5wzvce";
+    hash = "sha256-I1a+wR5zNfOSYyns2ehDek5DP1xccRQvmBLymxB/R7A=";
   };
 
   firefox = null;
@@ -15,9 +15,12 @@
   orbstack = null;
 
   slack = {
-    version = "4.50.143";
-    hash = "sha256-f5oYi8NPKXWpDcowFYMUjptbQs4V9hK2bm1TuaQeJn0=";
+    version = "4.51.180";
+    hash = "sha256-6QhuZWEwQiW8ZyYTJRLMZq5MayBgX9xlBLdhttiTaoA=";
   };
 
-  zoom-us = null;
+  zoom-us = {
+    version = "7.1.5.84650";
+    hash = "sha256-Sc9wr2dBdvNwi507UZ9YVCTEQsz3RP2WWTJ5Z5ccgnk=";
+  };
 }
