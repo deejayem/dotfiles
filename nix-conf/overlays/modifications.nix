5ed9c72f66960020d47458285b2b45d3a5b48ea6
David Morgan <djm_uk@protonmail.com>
2026-07-27 08:32:30 +0100

    Update atuin patches

--- nix-conf/overlays/modifications.nix
+++ nix-conf/overlays/modifications.nix
@@ -7,7 +7,7 @@ in
   atuin = prev.atuin.overrideAttrs (oldAttrs: {
     patches = (oldAttrs.patches or [ ]) ++ [
       (
-        if prev.lib.versionAtLeast prev.atuin.version "18.13.0" then
+        if prev.lib.versionAtLeast prev.atuin.version "18.17.1" then
           ./atuin-session-host-filter-unstable.patch
         else
           ./atuin-session-host-filter.patch
