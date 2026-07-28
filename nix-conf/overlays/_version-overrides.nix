b3812975e25f4190a1d709578746707b5c081cab
David Morgan <djm_uk@protonmail.com>
2026-07-28 09:55:06 +0100

    Update version overrides

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -1,23 +1,16 @@
 {
-  brave = {
-    version = "1.92.143";
-    hash = "sha256-EvfZgO8FAijof1Ml6gqSOyRndL8KYFdT0MNmVmuxAnU=";
-  };
+  brave = null;
 
-  google-chrome = {
-    version = "150.0.7871.187";
-    slug = "ecziwrw2etq4lm3vlbpv5wzvce";
-    hash = "sha256-I1a+wR5zNfOSYyns2ehDek5DP1xccRQvmBLymxB/R7A=";
-  };
+  google-chrome = null;
 
-  firefox = null;
+  firefox = {
+    version = "153.0.1";
+    sha512 = "107zghz81m526y5h9phhkdamx21ihqz4iqmrhflkwgcmx2lnvsh2j0d77lhjgz5zvmdsvsrdrghz7c1savy6qi90mgfy0krgfi1l39i";
+  };
 
   orbstack = null;
 
-  slack = {
-    version = "4.51.180";
-    hash = "sha256-6QhuZWEwQiW8ZyYTJRLMZq5MayBgX9xlBLdhttiTaoA=";
-  };
+  slack = null;
 
   zoom-us = {
     version = "7.1.5.84650";
