f1a6deab569aff7ab04417595216d940907b8572
David Morgan <djm_uk@protonmail.com>
2026-08-10 08:41:25 +0100

    Update flake lockfile

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -4,16 +4,9 @@
     hash = "sha256-0xf6LZ7tbmW8I3rDIt64yOl1v59YpBL0N/QZQLCcb0w=";
   };
 
-  google-chrome = {
-    version = "151.0.7922.109";
-    slug = "cy2fmmk4db26pfgaa3nc5udp2i";
-    hash = "sha256-CqLJP/LSSX6lKkqBqYCbcIy9GLJGVGVmzH8pHYEsJoU=";
-  };
+  google-chrome = null;
 
-  firefox = {
-    version = "153.0.3";
-    sha512 = "2y28j46bbd351i53apk5f1id3yk7h931kywq9kkysmadl2w5g5w3wksg4zzdj79kla5k4ai574njhkjlbbjfpmlrvr3vxcl3k2k17s4";
-  };
+  firefox = null;
 
   orbstack = {
     version = "2.2.3-20963";
