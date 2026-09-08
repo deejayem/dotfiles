327018b866c30e03395537b57785c89faf0b9089
David Morgan <djm_uk@protonmail.com>
2026-09-08 17:21:22 +0100

    Remove obsolete version overrides

--- nix-conf/overlays/_version-overrides.nix
+++ nix-conf/overlays/_version-overrides.nix
@@ -1,19 +1,9 @@
 {
-  brave = {
-    version = "1.94.117";
-    hash = "sha256-OHPEARZoRW1/svXUTP+5rWTyNu6xOYisBkSJy+Z4Vzo=";
-  };
+  brave = null;
 
-  google-chrome = {
-    version = "152.0.7977.76";
-    slug = "fwccdneh3i55zgoy366y75r2ya";
-    hash = "sha256-VuYzRvaOH0YB/I1m1Agk+l4y4al1b4o4dZZnhm7J2PQ=";
-  };
+  google-chrome = null;
 
-  firefox = {
-    version = "155.0";
-    sha512 = "1m0vklabii6m825qs4bnhfjr0a30ap0f6ch33phisy87s9xf9jqb0s3m54jniqf9wqbmh58qfxrl0wlk6qsfxzbjh29znsfja0jca94";
-  };
+  firefox = null;
 
   orbstack = null;
 
